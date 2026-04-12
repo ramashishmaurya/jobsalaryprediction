8554ef432e21cc95a56f20ae0b947352b50c4be8
ramashishmaurya <ramashishmaurya738@gmail.com>
2026-04-12 13:55:07 +0530

    made the project using the ml model

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from fastapi import FastAPI 
-from pathlib import Path
 from pydantic import BaseModel
 from typing import Literal
 import joblib
 import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware  # ✅ add karo
 
-import joblib
-
-model =joblib.load('./model.pkl')
-
-
+model = joblib.load('./models.pkl')
 
 app = FastAPI() 
 
+# ✅ Ye block add karo
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class EmployeeData(BaseModel):
-    job_title: Literal["Backend Developer" ,"Cybersecurity Analyst"]
+    job_title: Literal["Backend Developer", "Cybersecurity Analyst" ,'Product Manager','AI Engineer ','Data Scientist','DevOps Engineer','Software Engineer','Data Analyst ','Cloud Engineer ','Machine Learning Engineer','Business Analyst','Frontend Developer']
     experience_years: int
     education_level: str
     skills_count: int
@@ -26,14 +31,15 @@ class EmployeeData(BaseModel):
 
 @app.get('/')
 def home():
-    return{'message':'Salary prediction API is running'}
+    return {'message': 'Salary prediction API is running'}
 
 @app.post('/predict')
-def predict(data:EmployeeData):
+def predict(data: EmployeeData):
     data = data.model_dump()
-    input_df = pd.DataFrame([data])[0]
-    prediction = model.predict(input_df)[0]
+    input_df = pd.DataFrame([data])   
+
+    prediction = model.predict(input_df)
 
     return {
-        "predicted_salary": prediction
+        "predicted_salary": prediction.item()
     }
